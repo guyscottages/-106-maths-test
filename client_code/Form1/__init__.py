9acc4be88fb31ced967fc6c9352ea6673c0d0e0b
guyscottages@gmail.com <guyscottages@gmail.com>
2025-05-22 19:44:45 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,22 +4,27 @@ import random
 
 class Form1(Form1Template):
 
-  def __init__(self, **properties):
-    self.init_components(**properties)
+  def reset_operands(self):
     self.label_2.text = str(random.randint(0,9))
     self.label_3.text = str(random.randint(0,9))
-    self.drop_down_1.selected_value = random.choice(['+','-','*','/'])
+    self.drop_down_1.selected_value = random.choice(['+','-','*','/'])    
+    self.text_box_1.text = ''
+    self.text_box_1.focus()
+
+  def __init__(self, **properties):
+    self.init_components(**properties)
+    self.reset_operands()
 
   def button_1_click(self, **event_args):
     answertext = (
-      self.label_2.text + " " + 
-      self.drop_down_1.selected_value + " " + 
+      self.label_2.text + ' ' + 
+      self.drop_down_1.selected_value + ' ' + 
       self.label_3.text
     )
     answer = str(eval(answertext))
     if self.text_box_1.text == answer:
-      alert('Correct')
+      alert('Correct!')
     else:
       alert('Incorrect')
-
+    self.reset_operands()
       
