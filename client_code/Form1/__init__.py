664f7e86e6385d836a5c4c68e5682f231a1f60bb
guyscottages@gmail.com <guyscottages@gmail.com>
2025-05-22 19:42:36 +0000

    Created LICENSE.txt; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,25 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import random 
 
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+  def __init__(self, **properties):
+    self.init_components(**properties)
+    self.label_2.text = str(random.randint(0,9))
+    self.label_3.text = str(random.randint(0,9))
+    self.drop_down_1.selected_value = random.choice(['+','-','*','/'])
+
+  def button_1_click(self, **event_args):
+    answertext = (
+      self.label_2.text + " " + 
+      self.drop_down_1.selected_value + " " + 
+      self.label_3.text
+    )
+    answer = str(eval(answertext))
+    if self.text_box_1.text == answer:
+      alert('Correct')
+    else:
+      alert('Incorrect')
+
+      
